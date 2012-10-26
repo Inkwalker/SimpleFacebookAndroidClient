73cfa08eaff38d5b61333ac8c57baa58f5229681
CockroachSlidy <cockroachslidy@gmail.com>
2012-10-26 17:26:58 +0300

    proper login and logout screens

--- src/com/nemo/fbdemo/MainActivity.java
+++ src/com/nemo/fbdemo/MainActivity.java
@@ -1,48 +1,185 @@
 package com.nemo.fbdemo;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
+
 import com.facebook.FacebookActivity;
-import com.facebook.Request;
+import com.facebook.LoginFragment;
+import com.facebook.Session;
 import com.facebook.SessionState;
-import com.facebook.GraphUser;
-import com.facebook.Response;
-import android.widget.TextView;
 
 public class MainActivity extends FacebookActivity {
+	
+	private static final String TAG = "Simple FB Client";
+	
+	private static final int SPLASH = 0;
+	private static final int SELECTION = 1;
+	private static final int SETTINGS = 2;
+	private static final int FRAGMENT_COUNT = SETTINGS + 1;
+
+	private static final String FRAGMENT_PREFIX = "fragment";
+	
+	private boolean isResumed = false;
+	private boolean restoredFragment = false;
+	private Fragment[] fragments = new Fragment[FRAGMENT_COUNT];
+	
+	private MenuItem settings;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.openSession();
+        
+        for(int i = 0; i < fragments.length; i++) {
+            restoreFragment(savedInstanceState, i);
+        }
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        FragmentManager manager = getSupportFragmentManager();
+        Fragment currentFragment = manager.findFragmentById(R.id.body_frame);
+        // only add the menu when the selection fragment is showing
+        if (currentFragment == fragments[SELECTION]) {
+            if (menu.size() == 0) {
+                settings = menu.add(R.string.settings);
+            }
+            return true;
+        } else {
+            menu.clear();
+            settings = null;
+        }
+        return false;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.equals(settings)) {
+            FragmentManager manager = getSupportFragmentManager();
+            FragmentTransaction transaction = manager.beginTransaction();
+            transaction.add(R.id.body_frame, 
+                            fragments[SETTINGS]).addToBackStack(null).commit();
+            return true;
+        }
+        return false;
+    }
+    
+    @Override
+    public void onResume() {
+        super.onResume();
+        isResumed = true;
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        isResumed = false;
     }
     
     @Override
     protected void onSessionStateChange(SessionState state, Exception exception) {
-    	// user has either logged in or not ...
-    	  if (state.isOpened()) {
-    	    // make request to the /me API
-    	    Request request = Request.newMeRequest(
-    	      this.getSession(),
-    	      new Request.GraphUserCallback() {
-    	        // callback after Graph API response with user object
-    	        @Override
-    	        public void onCompleted(GraphUser user, Response response) {
-    	          if (user != null) {
-    	            TextView welcome = (TextView) findViewById(R.id.welcome);
-    	            welcome.setText("Hello " + user.getName() + "!");
-    	          }
-    	        }
-    	      }
-    	    );
-    	    Request.executeBatchAsync(request);
-    	  }
+        if (isResumed) {
+            FragmentManager manager = getSupportFragmentManager();
+            int backStackSize = manager.getBackStackEntryCount();
+            for (int i = 0; i < backStackSize; i++) {
+                manager.popBackStack();
+            }
+            if (state.isOpened()) {
+                FragmentTransaction transaction = manager.beginTransaction();
+                transaction.replace(R.id.body_frame, 
+                                    fragments[SELECTION]).commit();
+            } else if (state.isClosed()) {
+                FragmentTransaction transaction = manager.beginTransaction();
+                transaction.replace(R.id.body_frame, 
+                                    fragments[SPLASH]).commit();
+            }
+        }
+    }
+    
+    private String getBundleKey(int index) {
+        return FRAGMENT_PREFIX + Integer.toString(index);
+    }
+
+    private void restoreFragment(Bundle savedInstanceState, int fragmentIndex) {
+        Fragment fragment = null;
+        if (savedInstanceState != null) {
+            FragmentManager manager = getSupportFragmentManager();
+            fragment = manager.getFragment(savedInstanceState, 
+                       getBundleKey(fragmentIndex));
+        }
+        if (fragment != null) {
+            fragments[fragmentIndex] = fragment;
+            restoredFragment = true;
+        } else {
+            switch (fragmentIndex) {
+                case SPLASH:
+                    fragments[SPLASH] = new SplashFragment();
+                    break;
+                case SELECTION:
+                    fragments[SELECTION] = new SelectionFragment();
+                    break;
+                case SETTINGS:
+                    fragments[SETTINGS] = new LoginFragment();
+                    break;
+                default:
+                    Log.w(TAG, "invalid fragment index");
+                    break;
+            }
+        }
+    }
+    
+    @Override
+    protected void onResumeFragments() {
+        super.onResumeFragments();
+        Session session = Session.getActiveSession();
+        if (session == null || session.getState().isClosed()) {
+            session = new Session(this);
+            Session.setActiveSession(session);
+        }
+
+        FragmentManager manager = getSupportFragmentManager();
+
+        if (restoredFragment) {
+            return;
+        }
+
+        // If we already have a valid token, then we can just open the session silently,
+        // otherwise present the splash screen and ask the user to login.
+        if (session.getState().equals(SessionState.CREATED_TOKEN_LOADED)) {
+            // no need to add any fragments here since it will be 
+            // handled in onSessionStateChange
+            session.openForRead(this);
+        } else if (session.isOpened()) {
+            // if the session is already open, try to show the selection fragment
+            Fragment fragment = manager.findFragmentById(R.id.body_frame);
+            if (!(fragment instanceof SelectionFragment)) {
+                manager.beginTransaction().replace(R.id.body_frame, 
+                        fragments[SELECTION]).commit();
+            }
+        } else {
+            FragmentTransaction transaction = manager.beginTransaction();
+            transaction.replace(R.id.body_frame, 
+                  fragments[SPLASH]).commit();
+        }
+    }
+    
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        FragmentManager manager = getSupportFragmentManager();
+        // Since we're only adding one Fragment at a time, we 
+        // can only save one.
+        Fragment f = manager.findFragmentById(R.id.body_frame);
+        for (int i = 0; i < fragments.length; i++) {
+            if (fragments[i] == f) {
+                manager.putFragment(outState, 
+                        getBundleKey(i), fragments[i]);
+            }
+        }
     }
 }
